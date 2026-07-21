e46b60b67218c32c3d346a7b0ad2ef5b975ee9a8
rith-me <gbol7415@gmail.com>
2026-07-21 13:10:48 +0700

    feat: set basePath for routing and asset URLs in Next.js configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  // Adds your repository name as a prefix to all routing and asset URLs
+  basePath: '/invoice-app',
   images: {
     unoptimized: true,
   },
