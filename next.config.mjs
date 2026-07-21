5dd3c186e8f178bb9a1fc2fc199a3df374ef2112
rith-me <gbol7415@gmail.com>
2026-07-21 16:05:16 +0700

    fix: update Next.js configuration formatting and ensure proper structure

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-     output: 'export',
-     basePath:'',
-     images: { unoptimized: true },
-   };
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;
